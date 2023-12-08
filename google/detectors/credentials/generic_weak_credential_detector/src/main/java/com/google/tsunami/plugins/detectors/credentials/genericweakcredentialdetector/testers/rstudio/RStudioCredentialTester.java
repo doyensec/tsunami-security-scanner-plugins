41b34e952f732e3d77439415c5f63d36d1d76e6a
leonardo <leonardo@ip-192-168-178-180.us-west-2.compute.internal>
2023-12-08 17:13:19 +0100

    Updataed rstudio plugin

--- google/detectors/credentials/generic_weak_credential_detector/src/main/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/rstudio/RStudioCredentialTester.java
+++ google/detectors/credentials/generic_weak_credential_detector/src/main/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/rstudio/RStudioCredentialTester.java
@@ -15,6 +15,43 @@
  */
 package com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.testers.rstudio;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.tsunami.common.net.http.HttpRequest.get;
+import static com.google.tsunami.common.net.http.HttpRequest.post;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Optional;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.flogger.GoogleLogger;
+import com.google.protobuf.ByteString;
+import com.google.tsunami.common.data.NetworkEndpointUtils;
+import com.google.tsunami.common.data.NetworkServiceUtils;
+import com.google.tsunami.common.net.http.HttpClient;
+import com.google.tsunami.common.net.http.HttpHeaders;
+import com.google.tsunami.common.net.http.HttpResponse;
+import com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.provider.TestCredential;
+import com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.tester.CredentialTester;
+import com.google.tsunami.proto.NetworkService;
+import javax.inject.Inject;
 
 public final class RStudioCredentialTester extends CredentialTester {
     private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
@@ -68,7 +105,7 @@ public final class RStudioCredentialTester extends CredentialTester {
             HttpResponse response = httpClient.send(get(url).withEmptyHeaders().build());
             canAcceptByCustomFingerprint =
                 response.status().isSuccess()
-                    && response.headers().firstValue(SERVER_HEADER).equals(RSTUDIO_HEADER)
+                    && response.headers().firstValue(SERVER_HEADER).equals(RSTUDIO_HEADER);
         } catch (IOException e) {
             logger.atWarning().withCause(e).log("Unable to query '%s'.", url);
             return false;
@@ -91,31 +128,63 @@ public final class RStudioCredentialTester extends CredentialTester {
         try {
             logger.atInfo().log("Retrieving public key");
             HttpResponse response = httpClient.send(get(url).withEmptyHeaders().build());
-            String body = response.body()
-            String exp = body.split(":")[0]
-            String mod = body.split(":")[1] 
-            logger.atInfo().log("Exp: %s, Mod: %s", exp, mod);   
+            Optional<String> body = response.bodyString();
+            String exponent = body.get().split(":")[0];
+            String modulus = body.get().split(":")[1];
+            logger.atInfo().log("Exp: %s, Mod: %s", exponent, modulus);   
             
             url = buildTargetUrl(networkService, "/auth-do-sign-in");
             logger.atInfo().log(
                 "url: %s, username: %s, password: %s",
                 url, credential.username(), credential.password().orElse(""));
-            HttpResponse response = sendRequestWithCredentials(url, credential, exp, mod);
+            response = sendRequestWithCredentials(url, credential, exponent, modulus);
             
-            return response.status().isSuccess()
-                && response
-                .bodyString()
-                .map(RStudioCredentialTester::bodyContainsSuccessfulLoginElements)
-                .orElse(false);
+            return response.status().isRedirect()
+                && response.headers().get("Set-Cookie").isPresent();
         } catch (IOException e) {
             logger.atWarning().withCause(e).log("Unable to query '%s'.", url);
             return false;
         }
     }
 
-    private HttpResponse sendRequestWithCredentials(String url, TestCredential credential, String exp, String mod){
-        
-    }
+    private HttpResponse sendRequestWithCredentials(String url, TestCredential credential, String exponent, String modulus)
+        throws NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, InvalidKeySpecException, IOException{
+        //encrypting with RSA PCKS#1 version 2
+        RSAPublicKeySpec spec = new RSAPublicKeySpec(
+            new BigInteger(modulus,16), 
+            new BigInteger(exponent,16));
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        RSAPublicKey key = (RSAPublicKey) factory.generatePublic(spec);
 
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(credential.username());
+        sb.append("\n");
+        sb.append(credential.password());
+        byte[] cipherData = cipher.doFinal(sb.toString().getBytes());
+
+        //converting the ciphertext to hex
+        sb = new StringBuilder();
+        for (byte b : cipherData) {
+            sb.append(String.format("%02X ", b));
+        }
+
+        //now converting the hex to base64
+        byte[] data = Base64.encodeBase64(sb.toString().getBytes());
+        String ciphertext = new String(data, StandardCharsets.UTF_8);
+        logger.atInfo().log("Encrypted. Ciphertext %s", ciphertext);
+
+        var headers = HttpHeaders.builder()
+            .addHeader("Cookie", "rs-csrf-token=1")
+            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+            .build();
+
+        sb = new StringBuilder();
+        sb.append("rs-csrf-token=1&");
+        sb.append("v="+ciphertext);
+        return httpClient.send(post(url).setHeaders(headers).setRequestBody(ByteString.copyFrom(sb.toString().getBytes())).build());
+    }
 
 }
